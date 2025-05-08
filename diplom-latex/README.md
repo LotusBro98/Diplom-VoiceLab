d83649c7e9553fead21229967b13b5b4787e280c
Lotusbro98 <lotusbro98@gmail.com>
2025-05-08 15:55:23 +0300

    diplom template

---
+++ diplom-latex/README.md
@@ -0,0 +1,26 @@
+# Магистерский диплом МФТИ, оформленный в LaTeX по стандартам ГОСТ
+
+Тема: Устойчивое представление аудиосигналов для задач синтеза речи \
+Автор: Панюков А.Д.\
+Научный руководитель: Горбацевич В.С.\
+Выпуск: МФТИ, ФРКТ 2025, группа М01-306б 
+
+Заготовка LaTeX принадлежит [shonenkov](https://github.com/shonenkov/masters-degree-diploma/)
+
+## Installing Linux ##
+1) INSTALL TEX LIVE
+```
+sudo apt-get install texlive-full
+```
+2) INSTALL TEXMAKER
+```
+sudo apt-get install texmaker
+```
+3) INSTALL CYRFONTS
+```
+sudo apt-get install scalable-cyrfonts-tex
+```
+## Usage ##
+```
+sh build.sh
+```
---
+++ diplom-latex/build.sh
@@ -0,0 +1,32 @@
+
+# Pdflatex
+PDFLATEX='pdflatex -interaction=nonstopmode -shell-escape -file-line-error'
+
+# Файлы/Папки
+PDF_NAME='diploma.pdf'
+TEX='tex'
+IMG='tex/figures'
+MAINTEX='0-main'
+
+# Конвертация eps
+find $IMG/ -type f -name "*.eps" -exec epstopdf {} ";" ;
+find $IMG -type f -name "*.eps" -exec rm -f {} \;
+
+# Сборка latex
+cd tex
+$PDFLATEX $MAINTEX > /dev/null
+BIBOUTPUT=$(bibtex $MAINTEX)
+# Показывать output bibtex'a только в случае ошибок
+if [[ "$BIBOUTPUT" =~ "error" ]]; then
+    echo "$BIBOUTPUT"
+fi
+$PDFLATEX $MAINTEX > /dev/null
+$PDFLATEX $MAINTEX
+
+cp $MAINTEX.pdf ../$PDF_NAME
+cd ..
+
+# Clear
+find $TEX/ -maxdepth 1 -type f -regex ".*\.\(log\|aux\|bbl\|blg\|out\|toc\|pdf\)" -exec rm -f {} \; ;
+
+
